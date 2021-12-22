23725b21bfa5764979cfff4076b5229564a91dfb
Puppok <kravchenko19971808@gmail.com>
2021-12-22 08:51:46 +0800

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StorePageComponent } from './store-page/store-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent },
+  { path: 'store', component: StorePageComponent },
+  { path: 'cart', component: CartPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StorePageComponent } from './store-page/store-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent,
+    StorePageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
