1a48d3b3d21ceb9ba02d4fb74de103dd9b4f4dcf
ivan.kravchenko <kravchenko19971808@gmail.com>
2021-12-26 19:31:55 +0800

    refactored books service

--- src/app/shared/books.service.ts
+++ src/app/shared/books.service.ts
@@ -1,18 +1,47 @@
+import { APIService } from './API.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
-import { Info } from './books.interface';
+import { Book, Info } from './books.interface';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class BooksService {
 
-  constructor(private http: HttpClient) { }
+  info: Info = {
+    error: '',
+    total: '',
+    books: []
+  }
 
-  loadInfo():Observable<Info> {
-    const url = '../../assets/books.json'
-    return this.http.get<Info>(url)
+  private books$$ = new BehaviorSubject<Book[]>([])
+  readonly books$ = this.books$$.asObservable()
+
+  constructor(private apiService: APIService) { }
+
+  loadInfo() {
+    this.apiService.loadInfo().subscribe(info => this.info = info)
+  }
+
+  loadBooks() {
+    this.apiService.loadInfo()
+    .pipe(map(() => this.info.books))
+    .subscribe(books => this.books$$.next(books))
+  }
+
+  init() {
+    this.loadInfo()
+    this.loadBooks()
+  }
+
+  updateBook(id: string, book: Book) {
+    const bookList = this.books$$.getValue()
+    const outerBook = bookList.map(innerBook => {
+      if(innerBook.isbn13 === id) {
+        return {...innerBook, ...book}
+      }
+      return innerBook
+    })
+    this.books$$.next(outerBook)
   }
 }
