92f6114675cba1b7a5b46d8eb533b72f8e172b1b
ivan.kravchenko <kravchenko19971808@gmail.com>
2021-12-26 19:35:16 +0800

    refactored loading books from api

--- src/app/store-page/store-page.component.ts
+++ src/app/store-page/store-page.component.ts
@@ -1,38 +1,12 @@
-import { Info, Books } from '../shared/books.interface';
+import { Component } from '@angular/core';
 import { BooksService } from '../shared/books.service';
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'app-store-page',
   templateUrl: './store-page.component.html',
   styleUrls: ['./store-page.component.scss']
 })
-export class StorePageComponent implements OnInit {
+export class StorePageComponent {
 
-  books: Books[] = []
-
-  info: Info = {
-    error: '',
-    total: '',
-    books: []
-  }
- 
-  constructor(private booksService: BooksService) { }
-
-  ngOnInit(): void {
-    this.booksService.loadInfo().subscribe(info => {
-      this.info = info
-      console.log(info)
-    })
-
-    this.booksService.loadInfo()
-    .pipe(
-      map(() => this.info.books)
-    )
-    .subscribe(books => {
-      this.books = books
-      console.log(this.books)
-    })
-  }
+  constructor(readonly booksService: BooksService) { }
 }
