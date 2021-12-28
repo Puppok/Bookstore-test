b66d4f229a72491dd7b4cab3b4eadd82edf17c64
Puppok <kravchenko19971808@gmail.com>
2021-12-28 19:31:52 +0800

    finish register-page component

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { debounceTime } from 'rxjs';
 import { User } from '../shared/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -13,11 +14,14 @@ export class RegisterPageComponent implements OnInit {
   register: FormGroup = new FormGroup({})
   users: User[] = []
 
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
     this.register = new FormGroup({
       login: new FormControl(''),
       password: new FormControl('')
     })
+    localStorage.setItem('Users', JSON.stringify([{login: 'admin', password: 'admin'}]))
   }
 
   onSubmit() {
@@ -27,6 +31,7 @@ export class RegisterPageComponent implements OnInit {
       password: this.register.value.password
     }
     this.saveData([...test, user])
+    this.router.navigateByUrl('/auth')
   }
 
   saveData(users: User[]) {
