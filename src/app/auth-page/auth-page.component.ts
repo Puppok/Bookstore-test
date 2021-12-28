4c311ef8096e67511b7d74c68ca509ecee79bbf9
Puppok <kravchenko19971808@gmail.com>
2021-12-28 19:53:36 +0800

    finish auth-page component

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -2,6 +2,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../shared/user.interface';
 import { v4 as uuidv4 } from 'uuid'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-page',
@@ -11,13 +12,15 @@ import { v4 as uuidv4 } from 'uuid'
 export class AuthPageComponent implements OnInit {
 
   auth: FormGroup = new FormGroup({})
+  validData: boolean = false
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.auth = new FormGroup({
       login: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required])
     })
-    localStorage.setItem('Users', JSON.stringify([{login: 'admin', password: 'admin'}]))
   }
 
   private generateToken(): void {
@@ -29,13 +32,15 @@ export class AuthPageComponent implements OnInit {
   onSubmit() {    
     const findUser = this.loadData().find(user => {
       return user.login === this.auth.value.login && user.password === this.auth.value.password
-    });
-    console.log(findUser);
+    })
     
     if(findUser){
       this.generateToken();
+      this.validData = true
+      setTimeout(() => {this.router.navigateByUrl('/home')}, 1500)
     } else {
-      this.auth.setErrors({invalid_data: true});
+      this.auth.setErrors({invalid_data: true})
+      setTimeout(() => {this.router.navigateByUrl('/register')}, 1500)
     }
   }
 
