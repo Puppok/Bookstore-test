240a5285ceb23af3f6fb334ce259636debc22597
ivan.kravchenko <kravchenko19971808@gmail.com>
2021-12-28 13:00:12 +0800

    some fixes

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -15,7 +15,10 @@ export class BookDetailComponent implements OnInit {
 
   book?: Book
 
-  constructor(private route: ActivatedRoute, private booksService: BooksService, private router: Router, private cartService: CartService) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private booksService: BooksService,
+              private cartService: CartService) {}
 
   ngOnInit(): void {
     this.route.params.pipe(
@@ -29,23 +32,6 @@ export class BookDetailComponent implements OnInit {
         this.router.navigateByUrl('/store')
       }
     })
-    // this.booksService.loadInfo().subscribe(info => {
-    //   this.info = info
-    //   console.log(info)
-    // })
-
-    // this.booksService.loadInfo()
-    //   .pipe(
-    //     map(() => this.info.books.find(book => book.isbn13 == this.currentId)),
-    //     filter(Boolean)
-    //   )
-    //   .subscribe(book => {
-    //     this.book.title = book.title
-    //     this.book.price = book.price
-    //     this.book.subtitle = book.subtitle
-    //     this.book.image = book.image
-    //     console.log(this.book)
-    //   })
   }
 
   addToCart() {
